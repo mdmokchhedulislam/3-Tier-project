f29de89e02aad8398e30e8afff4fc4f855f24d46
DeveloperMokchhedul <mokchheduls46@gmail.com>
2025-08-17 12:08:50 +0600

    backend version update

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,18 +1,22 @@
+
+
+
 terraform {
-  required_version = "~> 1.9.3"
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.49.0"
+      source = "hashicorp/aws"
+      version = "6.9.0"
     }
   }
-  backend "s3" {
+    backend "s3" {
     bucket         = "terraformprojectbackend"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
     encrypt        = true
   }
+
+
 }
 
 provider "aws" {

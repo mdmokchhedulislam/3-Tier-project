4237843a942adcbd9e832ad8785dc0084be3a486
mokchhedulislam <mokchheduls46@gmail.com>
2026-03-05 10:02:32 +0600

    setup s3 without cloudfront

--- infra/terraform/modules/s3/main.tf
+++ infra/terraform/modules/s3/main.tf
@@ -18,14 +18,6 @@ resource "aws_s3_bucket_ownership_controls" "frontend" {
   }
 }
 
-
-
-resource "aws_cloudfront_origin_access_identity" "frontend_oai" {
-  comment = "${var.project_name}-OAI"
-}
-
-
-
 resource "aws_s3_bucket_policy" "frontend_policy" {
   bucket = aws_s3_bucket.frontend.id
 
@@ -33,12 +25,10 @@ resource "aws_s3_bucket_policy" "frontend_policy" {
     Version = "2012-10-17"
     Statement = [
       {
-        Effect = "Allow"
-        Principal = {
-          AWS = aws_cloudfront_origin_access_identity.frontend_oai.iam_arn
-        }
-        Action   = "s3:GetObject"
-        Resource = "${aws_s3_bucket.frontend.arn}/*"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = ["s3:GetObject"]
+        Resource  = "${aws_s3_bucket.frontend.arn}/*"
       }
     ]
   })
@@ -46,67 +36,93 @@ resource "aws_s3_bucket_policy" "frontend_policy" {
 
 
 
-resource "aws_cloudfront_distribution" "frontend_cdn" {
+# resource "aws_cloudfront_origin_access_identity" "frontend_oai" {
+#   comment = "${var.project_name}-OAI"
+# }
 
-  enabled             = true
-  is_ipv6_enabled     = true
-  default_root_object = "index.html"
 
-  origin {
-    domain_name = aws_s3_bucket.frontend.bucket_regional_domain_name
-    origin_id   = "S3-${aws_s3_bucket.frontend.id}"
 
-    s3_origin_config {
-      origin_access_identity = aws_cloudfront_origin_access_identity.frontend_oai.cloudfront_access_identity_path
-    }
-  }
+# resource "aws_s3_bucket_policy" "frontend_policy" {
+#   bucket = aws_s3_bucket.frontend.id
 
-  default_cache_behavior {
-    target_origin_id       = "S3-${aws_s3_bucket.frontend.id}"
-    viewer_protocol_policy = "redirect-to-https"
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect = "Allow"
+#         Principal = {
+#           AWS = aws_cloudfront_origin_access_identity.frontend_oai.iam_arn
+#         }
+#         Action   = "s3:GetObject"
+#         Resource = "${aws_s3_bucket.frontend.arn}/*"
+#       }
+#     ]
+#   })
+# }
 
-    allowed_methods = ["GET", "HEAD", "OPTIONS"]
-    cached_methods  = ["GET", "HEAD"]
 
-    forwarded_values {
-      query_string = false
-      cookies {
-        forward = "none"
-      }
-    }
 
-    min_ttl     = 0
-    default_ttl = 3600
-    max_ttl     = 86400
-  }
+# resource "aws_cloudfront_distribution" "frontend_cdn" {
 
-  custom_error_response {
-    error_code         = 403
-    response_code      = 200
-    response_page_path = "/index.html"
-  }
+#   enabled             = true
+#   is_ipv6_enabled     = true
+#   default_root_object = "index.html"
 
-  custom_error_response {
-    error_code         = 404
-    response_code      = 200
-    response_page_path = "/index.html"
-  }
+#   origin {
+#     domain_name = aws_s3_bucket.frontend.bucket_regional_domain_name
+#     origin_id   = "S3-${aws_s3_bucket.frontend.id}"
 
-  viewer_certificate {
-    cloudfront_default_certificate = true
-  }
+#     s3_origin_config {
+#       origin_access_identity = aws_cloudfront_origin_access_identity.frontend_oai.cloudfront_access_identity_path
+#     }
+#   }
 
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
+#   default_cache_behavior {
+#     target_origin_id       = "S3-${aws_s3_bucket.frontend.id}"
+#     viewer_protocol_policy = "redirect-to-https"
 
-  tags = {
-    Environment = "production"
-  }
+#     allowed_methods = ["GET", "HEAD", "OPTIONS"]
+#     cached_methods  = ["GET", "HEAD"]
 
-  depends_on = [
-    aws_s3_bucket_policy.frontend_policy
-  ]
-}
+#     forwarded_values {
+#       query_string = false
+#       cookies {
+#         forward = "none"
+#       }
+#     }
+
+#     min_ttl     = 0
+#     default_ttl = 3600
+#     max_ttl     = 86400
+#   }
+
+#   custom_error_response {
+#     error_code         = 403
+#     response_code      = 200
+#     response_page_path = "/index.html"
+#   }
+
+#   custom_error_response {
+#     error_code         = 404
+#     response_code      = 200
+#     response_page_path = "/index.html"
+#   }
+
+#   viewer_certificate {
+#     cloudfront_default_certificate = true
+#   }
+
+#   restrictions {
+#     geo_restriction {
+#       restriction_type = "none"
+#     }
+#   }
+
+#   tags = {
+#     Environment = "production"
+#   }
+
+#   depends_on = [
+#     aws_s3_bucket_policy.frontend_policy
+#   ]
+# }
